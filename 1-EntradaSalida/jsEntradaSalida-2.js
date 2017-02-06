2b7cf1e9f859222b0d52c62928b95a51aece4c95
denis0001 <francofl123@gmail.com>
2017-02-06 19:43:45 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,18 @@
 y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
 function Mostar()
 {
+	//Esto es un comenario de una línea.
+	/*Esto es un
+	comentario de 
+	varias 
+	líneas.
+	*/
+	//var nombre; //defino variable	
+	nombre = "Franco"; //cargo variable
+	nombre=prompt("Ingrese su nombre","Natalia Natalia"); //prompt: devuelve algo y se usa siempre con una varaible
+
+	alert("nombre: "+nombre); //concatenar
+	//var person = prompt("Ingresá tu nombre", "Natalia Natalia");
 	
 }
 
