3f03f9d1a746a5b5c401ec5bd4b284f6dc78bb7c
denis0001 <francofl123@gmail.com>
2017-02-07 12:06:39 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-4.js
+++ 1-EntradaSalida/jsEntradaSalida-4.js
@@ -4,6 +4,9 @@
 */
 function Mostar()
 {
+	var nombre;
+	nombre=prompt("Por favor ingresá tu nombre:");
+	document.getElementById('elNombre').value=nombre;
 	
 }
 
--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,6 +3,20 @@ transformarlos a enteros (parseInt) y Sumarlos.
 Mostar el resulto por medio de "ALERT"*/
 function sumar()
 {
+	var primernumero
+	var segundonumero
+	var resultado
+
+	primernumero=document.getElementById('numeroUno').value;
+	segundonumero=document.getElementById('numeroDos').value;
+
+	primernumero=parseInt(primernumero);
+	segundonumero=parseInt(segundonumero);
+
+	resultado=primernumero+segundonumero;
+
+	alert("La suma es "+resultado+".");
+
 
 }
 
