32d9075866a41f4642db92272ef31d8b77701caa
denis0001 <francofl123@gmail.com>
2017-02-13 20:19:18 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -2,7 +2,19 @@ function Mostrar()
 {
 //tomo la edad  
 
-	
+	var edad;
+	var estadocivil;
 
+	edad=document.getElementById('edad').value;
+	estadocivil=document.getElementById('estadoCivil').value;
+
+	if(edad<18)
+	{
+		if(estadocivil!=Soltero)
+		{
+			alert("Es muy pequeño para NO ser soltero.");
+		}
+	}
+	
 
 }//FIN DE LA FUNCIÓN
