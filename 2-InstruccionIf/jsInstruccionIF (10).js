94396771c783de1c07e62b565e9b32881658d474
denis0001 <francofl123@gmail.com>
2017-02-14 11:04:41 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,31 @@
 function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
+	var number;
+	number=Math.floor(Math.random() * 10) + 1;
+
+	if (number>8)
+	{
+		alert('Nota:'+number+' EXCELENTE');
+	}
+
+	else
+	{
+		if(number<4)
+		{
+				alert('Nota:'+number+' Vamos, la proxima se puede');
+		}
+		else
+		{
+				alert('Nota:'+number+' APROBÓ');
+		}
+	}
 	
+
+	
+	
+	
+
+
 
 }//FIN DE LA FUNCIÓN
