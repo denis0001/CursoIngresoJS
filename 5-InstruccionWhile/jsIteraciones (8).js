2cd44d9f4b9be292aaae685dafb1dc0d49f1f6a8
denis0001 <francofl123@gmail.com>
2017-02-16 20:29:41 -0300

    8

--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -1,11 +1,26 @@
 function Mostrar()
 {
-
-	var contador=0;
+	var numero
 	var positivo=0;
 	var negativo=1;
 	
 	var respuesta='si';
+
+	while(respuesta=='si')
+	{
+		numero=prompt('Ingrese numero');
+		numero=parseInt(numero);
+		if(numero<0)
+		{
+			negativo=negativo*numero;
+		}
+		else
+		{
+			positivo=positivo+numero;
+		}
+
+		respuesta=prompt('¿Desea seguir agregando números?')
+	}
 
 
 document.getElementById('suma').value=positivo;
