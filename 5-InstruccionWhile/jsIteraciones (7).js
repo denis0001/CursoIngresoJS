1eb7600488698c72af4f03c7e975db487bb44664
denis0001 <francofl123@gmail.com>
2017-02-16 20:04:35 -0300

    7

--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -1,9 +1,23 @@
 function Mostrar()
 {
-
+	var numero;
 	var contador=0;
 	var acumulador=0;
 	var respuesta='si';
+
+	while (respuesta=='si')
+	{
+		contador++;
+		numero=prompt('Ingrese numero');
+		numero=parseInt(numero);
+
+		respuesta=prompt('¿Desea seguir agregando numeros?');
+
+		acumulador=acumulador+numero;
+	}
+
+
+
 
 
 document.getElementById('suma').value=acumulador;
