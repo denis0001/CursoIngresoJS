03911c651ac0145ff255fe96baccf091b7fe6927
denis0001 <francofl123@gmail.com>
2017-02-20 21:00:11 -0300

    hasta el 10
    falta el 9

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -1,7 +1,19 @@
 function Mostrar()
 {
 
-	var contador=0;
+	var contador1=0;
+	var contador2=0;
+	var contadorceros=0;
+	var acumulador1=0;
+	var acumulador2=0;
+	var promnegativos;
+	var prompositivos;
+	var numero;
+	var negativos;
+	var positivos;
+	var ceros;
+	var pares;
+	var diferencia;
 	//declarar contadores y variables 
 	
 	var respuesta="si";
@@ -9,9 +21,45 @@ function Mostrar()
 	while(respuesta!="no")
 	{
 		
-	
-	}
+		numero=prompt('Ingrese numero');
+		numero=parseInt(numero);
 
+		if (numero>0)
+		{
+			contador1++;
+			acumulador1=acumulador1+numero;
+			respuesta=prompt('¿Desea seguir agregando numeros?');
+
+		}
+
+		if (numero==0)
+		{
+			contadorceros++;
+			respuesta=prompt('¿Desea seguir agregando numeros?');
+		}
+
+		if(numero<0)
+		{
+			contador2++;
+			acumulador2=acumulador2+numero;
+			respuesta=prompt('¿Desea seguir agregando numeros?');
+		}
+
+
+	}
+	prompositivos=acumulador1/contador1;
+	promnegativos=acumulador2/contador2;
+	diferencia=acumulador1-acumulador2;
+
+	document.write('1.Suma de los negativos= '+acumulador2+'<BR>'+ 
+		'2.Suma de los positivos= '+acumulador1+'<BR>'+ 
+		'3.Cantidad de positivos= '+contador1+'<BR>'+ 
+		'4.Cantidad de negativos= '+contador2+'<BR>'+ 
+		'5.Cantidad de ceros= '+contadorceros+'<BR>'+ 
+		'6.Cantidad numeros pares= '+'<BR>'+ 
+		'7.Promedio de positivos= '+prompositivos+'<BR>'+ 
+		'8.Promedio negativos= '+promnegativos+'<BR>'+ 
+		'9.Diferencia entre positivos y negativos= '+diferencia)
 
 
 
