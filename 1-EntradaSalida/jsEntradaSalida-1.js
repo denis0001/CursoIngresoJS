12ff6111f08732ee2a8b88a431b54c01502530a3
denis0001 <francofl123@gmail.com>
2017-02-28 19:08:07 -0300

    Add files via upload
    El primer js es el parcial y el segundo el recuperatorio.
    Parcial terminado y testeado en 2 horas.
    Recuperatorio en 1 hora 10 minutos.

--- 1-EntradaSalida/jsEntradaSalida-1.js
+++ 1-EntradaSalida/jsEntradaSalida-1.js
@@ -1,6 +1,266 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostar()
 {
-	alert("Esto funciona de maravilla");
+
+
+/*	var base;
+	var resultado;
+
+	base=document.getElementById('BASE').value;
+	base=parseInt(base);
+	resultado=base*4;
+	alert('El perimetro es: '+ resultado+'.');
+*/
+
+/*	var importe;
+	var IVA =21;
+	var importeMasIVA;
+
+	importe=prompt('Ingrese importe');
+	importe=parseInt(importe);
+
+
+
+	importeMasIVA=(importe*21)/100;
+
+	importeMasIVA= importeMasIVA+importe;
+
+	alert('El importe final es: '+importeMasIVA);
+*/
+
+
+/*	var largo;
+	var ancho;
+	var perimetro;
+	var alambre;
+
+	largo=document.getElementById('LARGO').value;
+	ancho=document.getElementById('ANCHO').value;
+
+	largo=parseInt(largo);
+	ancho=parseInt(ancho);
+
+	perimetro= (largo*2)+(ancho*2);
+
+	alambre=perimetro*3;
+
+	alert('Se necesitan '+alambre+' metros de alambre');
+*/
+
+/*	var numero1;
+	var numero2;
+	var resultado;
+
+	numero1=prompt('Ingrese el primer numero');
+	numero1=parseInt(numero1);
+
+	numero2= prompt('Ingrese el segundo numero');
+	numero2=parseInt(numero2);
+
+	if (numero1 == numero2)
+	{
+		resultado=numero1*numero2;
+	}
+
+	else
+		{
+			if (numero1>numero2)
+			{
+				resultado=numero1-numero2;
+			}
+
+			else
+			{
+				resultado=numero1+numero2;
+			}
+		}
+
+
+	document.write(resultado);*/
+
+/*
+	var dia;
+
+
+	dia=prompt('Ingrese día');
+
+
+	switch (dia)
+	{
+		case 'sabado':
+		case 'domingo':
+			alert('es fin de semana');
+			break;
+
+		default:
+			alert('a trabajar!!!!');
+			break;
+
+
+	}
+*/
+
+/*	var importe;
+	var mayorImporte=0;
+	var menorImporte=99999999999;
+
+	var contador=0;
+
+	while (contador<24)
+	{
+		contador++;
+
+		importe=prompt('Ingrese importe');
+		importe=parseInt(importe);
+
+		while(importe<1)
+		{
+			importe=prompt('Ingrese importe mayor a 0');
+			importe=parseInt(importe);
+		}
+
+		if (importe>mayorImporte)
+		{
+			mayorImporte=importe;
+		}
+
+		if (importe<menorImporte)
+		{
+			menorImporte=importe;
+		}
+
+	}
+
+
+	alert('El mayor importe fue: ' + mayorImporte + ' y el menor fue: ' + menorImporte);
+
+	
+*/
+
+
+/*	var nota;
+	var sexo;
+	var contador=0;
+	var promedioNotas;
+	var acumulador=0;
+	var promedioNotas
+	var notaMasBaja;
+	var contadorVaronesNotaMayor5=0;
+
+	while(contador<5)
+	{
+		
+
+		nota=prompt('Ingrese nota');
+		nota=parseInt(nota);
+
+		
+
+
+		while(nota<0 || nota>10 )
+		{
+			nota=prompt('Ingrese nota entre 0 y 10');
+			nota=parseInt(nota);
+		}
+
+		sexo=prompt('Ingrese sexo: "f" o "m" ');
+
+		while(!(sexo=='f' || sexo=='m'))
+		{
+			sexo=prompt('Ingrese sexo: "f" o "m" ')
+		}
+
+		acumulador=acumulador+nota;
+
+		if (contador==0)
+		{
+			notaMasBaja=nota;
+
+		}
+
+		if (nota<notaMasBaja)
+		{
+			notaMasBaja=nota;
+		}
+
+		if (sexo =='m' && nota>5)
+		{
+			contadorVaronesNotaMayor5++;
+		}
+		contador++;
+		
+	}
+	
+
+	promedioNotas=acumulador/contador;
+
+	alert('a- El promedio de notas totales es '+ promedioNotas);
+	alert('b- La nota más baja es: '+notaMasBaja);
+	alert('c- La cantidad de varones que su nota fue mayor o igual a 6 es: '+contadorVaronesNotaMayor5);
+*/
+
+	
+	var numero;
+	var acumulador=0;
+	var contador=0;
+	var contadorPares=0;
+	var numeroMaximo;
+	var numeroMinimo;
+	var respuesta='s';
+	var promedio;
+
+	while (respuesta=='s')
+	{	
+		numero=prompt('Ingrese numero positivo');
+		numero=parseInt(numero);
+
+		while(numero<0)
+		{
+			numero=prompt('Ingrese numero POSITIVO');
+			numero=parseInt(numero);
+		}
+		
+
+		if(numero%2==0)
+		{
+			contadorPares++;
+		}
+
+		if (contador==0)
+		{
+			numeroMinimo=numero;
+			numeroMaximo=numero;
+		}
+
+		else
+		{
+			if (numero<numeroMinimo)
+			{
+				numeroMinimo=numero;
+			}
+
+			if (numero>numeroMaximo)
+			{
+				numeroMaximo=numero;
+			}
+		}
+
+
+		contador++;
+		acumulador=acumulador+numero;
+
+		respuesta=prompt('¿Desea seguir agregando números? Ingrese "s" si lo desea');
+
+
+
+	}
+
+	promedio=acumulador/contador;
+
+	document.write('La cantidadde números pares es: '+ contadorPares+'<BR>'+
+					'El promedio de todos los números ingresados es: '+ promedio+'<BR>'+
+					'La suma de todos los números es: '+acumulador+'<BR>'+
+					'El número máximo es: '+numeroMaximo+' y el mínimo '+numeroMinimo)
+
 }
 
--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -9,11 +9,288 @@ function Mostar()
 	líneas.
 	*/
 	//var nombre; //defino variable	
-	nombre = "Franco"; //cargo variable
-	nombre=prompt("Ingrese su nombre","Natalia Natalia"); //prompt: devuelve algo y se usa siempre con una varaible
+	//nombre = "Franco"; //cargo variable
+	//nombre=prompt("Ingrese su nombre","Natalia Natalia"); //prompt: devuelve algo y se usa siempre con una varaible
 
-	alert("nombre: "+nombre); //concatenar
+	//alert("nombre: "+nombre); //concatenar
 	//var person = prompt("Ingresá tu nombre", "Natalia Natalia");
+
+/*
+	var base;
+	var resultado;
+
+	base=document.getElementById('BASE').value;
+	base=parseInt(base);
+
+	resultado=base*base;
+
+	Alert('El perimetro es '+resultado);
+
+*/
+
+/*
+	var importe;
+	var descuento;
+	var resultado;
+
+	importe=prompt('Ingrese importe');
+	importe=parseInt(importe);
+
+	descuento=(importe*25)/100;
+
+	resultado=importe-descuento;
+
+	alert('El importe final es: '+resultado);
+
+*/
+/*
+	var precio1;
+	var precio2;
+	var precio3;
+	var acumulador;
+	var promedio;
+
+	precio1=document.getElementById('PRECIO1').value;
+	precio1=parseInt(precio1);
+
+	precio2=document.getElementById('PRECIO2').value;
+	precio2=parseInt(precio2);
+
+	precio3=document.getElementById('PRECIO3').value;
+	precio3=parseInt(precio3);
+
+
+	acumulador=precio1+precio2+precio3;
+
+	promedio=acumulador/3;
+
+	alert('La suma de los precios es: ' + acumulador);
+	alert('El promedio de los precios es: '+promedio);
+
+*/
+/*
+	var numero1;
+	var numero2;
+	var resultado;
+
+	numero1=prompt('Ingrese el primer numero');
+	numero1=parseInt(numero1);
+	numero2=prompt('Ingrese el segundo numero');
+	numero2=parseInt(numero2);
+
+	resultado=numero1+numero2;
+
+	if (resultado==0)
+	{
+		document.write('Cero');
+	}
+
+	else
+	{
+		if (resultado>0)
+		{
+			document.write('Positivo');
+
+		}
+
+		else
+		{
+			document.write('Negativo')
+		}
+	}
+*/
+
+/*
+	var mes;
+
+	mes=prompt('Ingrese mes');
+
+	switch (mes)
+	{
+		case 'diciembre':
+			alert('Se vienen las fiestas');
+			break;
+
+		case 'enero':
+			alert('Comienza el año');
+			break;
+
+		default:
+			alert('No es enero, ni diciembre');
+
+	}
+
+*/		
+
+/*
+	var peso;
+	var masPesado;
+	var menosPesado;
+	var contador=0;
+
+	while (contador<50)
+	{
+		peso=prompt('Ingrese peso');
+		peso=parseInt(peso);
+
+		while(peso<1)
+		{
+			peso=prompt('Ingrese peso mayor a 0');
+			peso=parseInt(peso);
+		}
+
+
+		if (contador==0)
+		{
+			masPesado=peso;
+			menosPesado=peso;
+		}
+
+		else
+		{
+			if (peso>masPesado)
+			{
+				masPesado=peso;
+			}
+
+			if(peso<menosPesado)
+			{
+				menosPesado=peso;
+			}
+		}
+
+		contador++;
+
+	}
+
+
+	alert('El mas pesado fue '+masPesado);
+	alert('El menos pesado fue '+menosPesado);
+
+*/
+
+/*
+	var edad;
+	var sexo;
+	var contador=0;
+	var acumulador=0;
+	var promedio;
+	var edadMasBaja;
+	var varonesEdadMayor19=0;
+
+	while (contador<3)
+	{
+		edad=prompt('Ingrese edad');
+		edad=parseInt(edad);
+
+		while (edad<0 && edad>100)
+		{
+			edad=prompt('Ingrese edad entre 0 a 100');
+			edad=parseInt(edad);
+		}
+
+		sexo=prompt('Ingrese sexo: "m" o "f"')
+
+		while (!(sexo=='m' || sexo=='f' ))
+		{
+			sexo=prompt('Ingrese sexo: "m" o "f"')
+		}
+
+		if(contador==0)
+		{
+			edadMasBaja=edad;
+		}
+
+		else
+		{
+			if (edad<edadMasBaja)
+			{
+				edadMasBaja=edad;
+			}
+		}
+
+		if(sexo=='m' && edad>19)
+		{
+			varonesEdadMayor19++;
+		}
+
+		contador++;
+		acumulador=acumulador+edad;
+
+
+	}
+
+	promedio=acumulador/contador;
+
+	alert('El promedio de edad es ' + promedio);
+
+	alert('La edad mas baja es '+edadMasBaja);
+
+	alert('La cantidad de varones con edad mayor o igual a 20 es ' +varonesEdadMayor19);
+
+*/
+
+	var numero;
+	var numerosPares=0;
+	var acumulador=0;
+	var contador=0;
+	var promedio;
+	var numeroMaximo;
+	var numeroMinimo;
+	var respuesta='s';
+
+	while (respuesta=='s')
+	{
+		numero=prompt('Ingrese numero');
+		numero=parseInt(numero);
+
+		while(numero<0)
+		{
+			numero=prompt('Ingrese numero POSITIVO');
+			numero=parseInt(numero);
+		}
+
+		if (numero%2==0)
+		{
+			numerosPares++;
+		}
+
+		if(contador==0)
+		{
+			numeroMinimo=numero;
+			numeroMaximo=numero;
+		}
+
+		else
+		{
+			if (numero>numeroMaximo)
+			{
+				numeroMaximo=numero;
+			}
+
+			if (numero<numeroMinimo)
+			{
+				numeroMinimo=numero;
+			}
+		}
+
+		contador++;
+
+		acumulador=acumulador+numero;
+
+		respuesta=prompt('¿Desea seguir agregando numeros? Ingrese "s" si lo desea');
+
+	}
+
+	promedio=acumulador/contador;
+
+	document.write('La cantidad de numeros pares es '+numerosPares+'<BR>'+
+				'El promedio de todos los numeros ingresados es: '+ promedio+'<BR>'+
+				'La suma de todos los numeros es: '+acumulador+'<BR>'+
+				'El numero máximo es '+ numeroMaximo + ' y el numero mínimo '+numeroMinimo)
 	
+
+
+
 }
 
